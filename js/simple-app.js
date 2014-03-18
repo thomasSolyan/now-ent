ec6b6091f91c39803bc3786d23dcb7f17fff3bd9
metavurt <billy.oneal@tahoepartners.com>
2014-03-18 07:08:36 -0500

    MIA is ok

--- js/simple-app.js
+++ js/simple-app.js
@@ -22,7 +22,7 @@ $(function(){
 	fixPagesHeight()
 
 	//Init Pages
-	var pages = $('.swiper-pages').swiper()
+	var pages = $('.swiper-pages').swiper();
 
 	//Scroll Containers
 	$('.scroll-container').each(function(){
