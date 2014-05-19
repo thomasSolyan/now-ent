b5fca13039af4ca914c0fee233ad9eed1509fa55
tsolyan <tsolyan@tahoepartners.com>
2014-05-19 12:56:44 -0500

    generalize link names

--- js/slideMenu.js
+++ js/slideMenu.js
@@ -1,47 +1,53 @@
  (function() {
     var data = {
-            'urls': {'home': '#', 'projectsTeams': '#', 'employeeCenter': '#', 'workplaceServices': '#', 'company': '#', 'mySettings': '#', 'terms': '#', 'logout': '#', 'internet': '#', 'intranet': '#', 'extranet': '#', 'directory': '#', 'news': '#', 'events': '#', 'leadership': '#'},
+            'urls': {'link-1': '#', 'link-2': '#', 'link-3': '#', 'link-4': '#', 'link-5': '#', 'link-6': '#', 'link-7': '#', 'link-8': '#', 'link-9': '#', 'link-10': '#', 'link-11': '#', 'link-12': '#', 'link-13': '#', 'link-14': '#', 'link-15': '#'},
 
-            'copy': {'home': 'home', 'projectsTeams': 'projects & teamssssss', 'employeeCenter': 'employee center', 'workplaceServices': 'workplace services', 'company': 'company', 'mySettings': 'my settings', 'terms': 'terms and policies', 'logout': 'logout', 'internet': 'internet', 'intranet': 'intranet', 'extranet': 'extranet', 'directory': 'directory', 'news': 'news', 'events': 'events', 'leadership': 'leadership'},
+            'copy': {'link-1': 'home', 'link-2': 'projects & teams', 'link-3': 'employee center', 'link-4': 'workplace ssssservices', 'link-5': 'company', 'link-6': 'directory', 'link-7': 'news', 'link-8': 'events', 'link-9': 'leadership', 'link-10': 'my settings', 'link-11': 'terms and policies', 'link-12': 'logout', 'link-13': 'internet', 'link-14': 'intranet', 'link-15': 'extranet'},
 
-            'hide': {'home': '', 'projectsTeams': '', 'employeeCenter': '', 'workplaceServices': '', 'company': '', 'mySettings': '', 'terms': '', 'logout': '', 'internet': '', 'intranet': '', 'extranet': '', 'directory': '', 'news': '', 'events': '', 'leadership': ''},
+            //add 'invisible' to hide
+            'hide': {'link-1': '', 'link-2': '', 'link-3': '', 'link-4': '', 'link-5': '', 'link-6': '', 'link-7': '', 'link-8': '', 'link-9': '', 'link-10': '', 'link-11': '', 'link-12': '', 'link-13': '', 'link-14': '', 'link-15': ''},
         },
 
-        content = '<div id="nav-con" class="kill-pointer nav" data-id="nav-main" data-no><div class="mp-pusher" id="mp-pusher" data-no><nav id="mp-menu" class="mp-menu" data-no><div class="mp-level" data-no><ul data-no><li class="icon-global-menu"  data-trigger data-id="sec-open" data-no><div class="mp-level secondary" data-id="nav-sec" data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" title="Global Menu" data-no>Global Menu</h4></span><a class="mp-back" href="#" data-trigger data-id="sec-close" data-no>back</a><ul><li class="nav-left-panel border-white border-solo-bot {{hide.internet}}" data-no><a href="{{urls.internet}}" class="icon-inter" data-no>{{copy.internet}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.intranet}}" data-no><a href="{{urls.intranet}}" class="icon-intra" data-no>{{copy.intranet}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.extranet}}" data-no><a href="{{urls.extranet}}" class="icon-extra" data-no>{{copy.extranet}}</a></li></ul></div></li><li data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" data-no>intranet</h4></span></li><li class="nav-left-panel border-white border-solo-bot {{hide.home}}" data-no><a href="{{urls.home}}" class="icon-home" data-no>{{copy.home}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.projectsTeams}}" data-no><a href="{{urls.projectsTeams}}" class="icon-proj" data-no>{{copy.projectsTeams}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.employeeCenter}}" data-no><a href="{{urls.employeeCenter}}" class="icon-huma">{{copy.employeeCenter}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.workplaceServices}}" data-no><a href="{{urls.workplaceServices}}" class="icon-work" data-no>{{copy.workplaceServices}}</a></li><li class="nav-left-panel border-white border-solo-bot drop-down-custom {{hide.company}}" data-no><a href="{{urls.company}}" class="icon-comp" data-no>{{copy.company}}</a><ul class="mp-level-sub-drop bg-color-white" data-no><li class="nav-left-panel-sub {{hide.directory}}" data-no><a href="{{urls.directory}}" class="indicator-border-left" data-no>{{copy.directory}}</a></li><li class="nav-left-panel-sub {{hide.news}}" data-no><a href="{{urls.news}}" class="indicator-border-left" data-no>{{copy.news}}</a></li><li class="nav-left-panel-sub {{hide.events}}" data-no><a href="{{urls.events}}" class="indicator-border-left" data-no>{{copy.events}}</a></li><li class="nav-left-panel-sub {{hide.leadership}}" data-no><a href="{{urls.leadership}}" class="indicator-border-left" data-no>{{copy.leadership}}</a></li></ul></li><li data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white height-force-min-40" data-no></h4></span></li><li class="nav-left-panel border-white border-solo-bot {{hide.mySettings}}" data-no><a href="{{urls.mySettings}}" class="icon-sets" data-no>{{copy.mySettings}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.terms}}" data-no><a href="{{urls.terms}}" class="icon-term" data-no>{{copy.terms}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.internet}}" data-no><a href="{{urls.logout}}" class="icon-logs" data-no>{{copy.logout}}</a></li></ul></div></nav></div></div>',
+        content = '<div id="nav-con" class="kill-pointer nav" data-id="nav-main" data-no><div class="mp-pusher" id="mp-pusher" data-no><nav id="mp-menu" class="mp-menu" data-no><div class="mp-level" data-no><ul data-no><li class="icon-global-menu"  data-trigger data-id="sec-open" data-no><div class="mp-level secondary" data-id="nav-sec" data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" title="Global Menu" data-no>Global Menu</h4></span><a class="mp-back" href="#" data-trigger data-id="sec-close" data-no>back</a><ul><li class="nav-left-panel border-white border-solo-bot {{hide.link-13}}" data-no><a href="{{urls.link-13}}" class="icon-inter" data-no>{{copy.link-13}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-14}}" data-no><a href="{{urls.link-14}}" class="icon-intra" data-no>{{copy.link-14}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-15}}" data-no><a href="{{urls.link-15}}" class="icon-extra" data-no>{{copy.link-15}}</a></li></ul></div></li><li data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" data-no>intranet</h4></span></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-1}}" data-no><a href="{{urls.link-1}}" class="icon-home" data-no>{{copy.link-1}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-2}}" data-no><a href="{{urls.link-2}}" class="icon-proj" data-no>{{copy.link-2}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-3}}" data-no><a href="{{urls.link-3}}" class="icon-huma">{{copy.link-3}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-4}}" data-no><a href="{{urls.link-4}}" class="icon-work" data-no>{{copy.link-4}}</a></li><li class="nav-left-panel border-white border-solo-bot drop-down-custom {{hide.link-5}}" data-no><a href="{{urls.link-5}}" data-trigger data-id="hidden-nav" class="icon-comp" data-no>{{copy.link-5}}</a><ul class="mp-level-sub-drop bg-color-white" data-id="hidden-links" data-no><li class="nav-left-panel-sub {{hide.link-6}}" data-no><a href="{{urls.link-6}}" class="indicator-border-left" data-no>{{copy.link-6}}</a></li><li class="nav-left-panel-sub {{hide.link-7}}" data-no><a href="{{urls.link-7}}" class="indicator-border-left" data-no>{{copy.link-7}}</a></li><li class="nav-left-panel-sub {{hide.link-8}}" data-no><a href="{{urls.link-8}}" class="indicator-border-left" data-no>{{copy.link-8}}</a></li><li class="nav-left-panel-sub {{hide.link-9}}" data-no><a href="{{urls.link-9}}" class="indicator-border-left" data-no>{{copy.link-9}}</a></li></ul></li><li data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white height-force-min-40" data-no></h4></span></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-10}}" data-no><a href="{{urls.link-10}}" class="icon-sets" data-no>{{copy.link-10}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-11}}" data-no><a href="{{urls.link-11}}" class="icon-term" data-no>{{copy.link-11}}</a></li><li class="nav-left-panel border-white border-solo-bot {{hide.link-12}}" data-no><a href="{{urls.link-12}}" class="icon-logs" data-no>{{copy.link-12}}</a></li></ul></div></nav></div></div>',
 
         template = Handlebars.compile(content),
         html    = template(data),
         $idMain = $('[data-id=main]'),
 
-    openSidepage = function() {  
-        $('[data-id=nav-main]')
-            .animate({left: '310px'}, 400)
-            .removeClass('kill-pointer')
-            .css('top', '40px');
-        $idMain.addClass('open');
-    },
+        openSidepage = function() {  
+            var pageHeight = $('html').height() * 1.25;
+            $('[data-id=nav-main]')
+                .animate({left: '310px'}, 400)
+                .removeClass('kill-pointer')
+                .css({
+                    top: '40px',
+                    height: pageHeight + 'px'
+                });
+            $idMain.addClass('open');
+        },
 
-    openInnerpage = function() {  
-        $('[data-id=nav-sec]')
-            .animate({left: '310px'}, 400)
-            .removeClass('kill-pointer'); 
-    },
-  
-    closeSidepage = function (){        
-        $('[data-id=nav-main]')
-            .animate({left: '-310px'}, 400)
-            .addClass('kill-pointer');
-        closeInnerpage();
-        $idMain.removeClass('open');
-    },
+        openInnerpage = function() {  
+            $('[data-id=nav-sec]')
+                .animate({left: '310px'}, 400)
+                .removeClass('kill-pointer'); 
+        },
+      
+        closeSidepage = function (){        
+            $('[data-id=nav-main]')
+                .animate({left: '-310px'}, 400)
+                .addClass('kill-pointer');
+            closeInnerpage();
+            $idMain.removeClass('open');
+        },
+        closeInnerpage = function() {  
+            $('[data-id=nav-sec]')
+                .animate({left: '-310px'}, 400)
+                .addClass('kill-pointer');
+        };
 
-    closeInnerpage = function() {  
-        $('[data-id=nav-sec]')
-            .animate({left: '-310px'}, 400)
-            .addClass('kill-pointer');
-    };
 
     $('html').on('click', '[data-trigger]', function(ev) {
+        ev.preventDefault();
         var dataId = $(ev.target).data('id');        
         switch(dataId) {
             case 'main':
@@ -59,6 +65,9 @@
             case 'sec-close':
                 closeInnerpage();
                 break;
+            case 'hidden-nav':
+                $('[data-id=hidden-links]').slideToggle( "slow");
+                break;
         }
     });
      $('[data-target=template]').append(html);
@@ -70,6 +79,7 @@
             if ($idMain.hasClass('open')) {
                 closeSidepage();
                 $idMain.removeClass('open');
+                $('[data-id=hidden-links]').hide();
             }
         }        
     });
