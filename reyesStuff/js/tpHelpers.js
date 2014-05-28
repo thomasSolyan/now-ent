2d8e7f3398f8a1c19803163c6486fca93099c984
tsolyan <tsolyan@tahoepartners.com>
2014-05-28 12:42:27 -0500

    ember app

--- reyesStuff/js/tpHelpers.js
+++ reyesStuff/js/tpHelpers.js
@@ -1,8 +1,11 @@
-    Handlebars.registerHelper('dateFormat', function(val) {
-        var d = (new Date(val)).toString().split(' ').splice(1,4).join(' ');
-        return d;
-    });
+Handlebars.registerHelper('dateFormat', function(val) {
+	return (new Date(val)).toString().split(' ').splice(1,4).join(' ');
+});
 
-    Handlebars.registerHelper('visible', function(val) {
-        if (val === 'false' || !val) { return 'invisible' }
-    });
+Handlebars.registerHelper('visible', function(val) {
+	if (val === 'false' || !val) { return 'invisible' }
+});
+
+Handlebars.registerHelper('noMarkup', function(val) {
+    return val.replace(/(<([^>]+)>)/ig,"");
+});
