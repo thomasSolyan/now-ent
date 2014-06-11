0ca4c6cbf19ae3ee3351af7e7cf5f3ee80f35bbe
tsolyan <tsolyan@tahoepartners.com>
2014-06-11 07:16:45 -0500

    update Wed AM

--- ec-microsite/sitecore-theme/js/script.js
+++ ec-microsite/sitecore-theme/js/script.js
@@ -1,28 +1,17 @@
 
 $(document).ready(function () {
 
-    jQuery(function ($) {
-        $.supersized({
-            slide_interval: 7000,
-            transition: 1, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-            transition_speed: 600,
-            slide_links: 'false',
-            slides: [
-                {
-                    image: 'images/slider1.jpg',
-                    title: ' <h2>Love & Passionate <br> for What We Do</h2>'
-                }, {
-                    image: 'images/slider2.jpg',
-                    title: ' <h2>Design Wise <br> Simple & Amazing</h2>'
-                }, {
-                    image: 'images/slider3.jpg',
-                    title: '<h2>Integrity & Loyal <br> for Customer</h2>'
-                }, {
-                    image: 'images/slider4.jpg',
-                    title: '<h2>Bunch of Creative <br> People & Idea</h2>'
-                }
-            ]
-        });
+    $.supersized({
+        slide_interval: 7000,
+        transition: 1, // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+        transition_speed: 600,
+        slide_links: 'false',
+        slides: [
+            {image: 'images/slider1.jpg'},
+            {image: 'images/slider2.jpg'},
+            {image: 'images/slider3.jpg'},
+            {image: 'images/slider4.jpg'}
+        ]
     });
 
     $('.header').sticky({
@@ -53,16 +42,14 @@ $(document).ready(function () {
         deeplinking: false
     });
 
-    //portfolio scrolling
-    $(function () {
-        $('.more,.more,.cat-icon').bind('click', function (event) {
-            var $anchor = $('#workslist');
 
-            $('html, body').stop().animate({
-                scrollTop: $($anchor).offset().top - 89
-            }, 1000, 'linear');
-            event.preventDefault();
-        });
+    $('.more,.cat-icon').bind('click', function (event) {
+        var $anchor = $('#workslist');
+
+        $('html, body').stop().animate({
+            scrollTop: $($anchor).offset().top - 89
+        }, 1000, 'linear');
+        event.preventDefault();
     });
 
     //portfolio setting
@@ -70,16 +57,13 @@ $(document).ready(function () {
         interval: 6000
     });
 
-    //team scrolling
-    $(function () {
-        $('.team-ajax,.team-icon').bind('click', function (event) {
-            var $anchor = $('#teamlist');
+    $('.team-ajax,.team-icon').bind('click', function (event) {
+        var $anchor = $('#teamlist');
 
-            $('html, body').stop().animate({
-                scrollTop: $($anchor).offset().top - 89
-            }, 1000, 'linear');
-            event.preventDefault();
-        });
+        $('html, body').stop().animate({
+            scrollTop: $($anchor).offset().top - 89
+        }, 1000, 'linear');
+        event.preventDefault();
     });
 
 });
