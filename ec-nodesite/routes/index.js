b064050786aa4bc1b8ce2d5b7dea2c6c2ca513ea
tsolyan <tsolyan@tahoepartners.com>
2014-06-25 06:39:47 -0500

    wed am update

--- ec-nodesite/routes/index.js
+++ ec-nodesite/routes/index.js
@@ -1,13 +1,18 @@
 var express = require('express'),
     router = express.Router();
 
-/* GET home page. */
+/* GET Index page. */
 router.get('/', function(req, res) {
+    res.render('index', { title: 'ec-microsite' })
+});
+
+/* GET IT page. */
+router.get('/it-dev', function(req, res) {
   var db = req.db,
       collection = db.get('ecMicro');
   collection.find({},{},function(e, docs){
-    res.render('index', {
-      title: 'nodeSite',
+    res.render('it-dev', {
+      title: 'ec-microsite',
       ecData : docs
     });
   });
