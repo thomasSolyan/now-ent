142217c616a37a55f0c1ada630d76f7e40bb5a7c
tsolyan <tsolyan@tahoepartners.com>
2014-07-01 10:51:26 -0500

    cleanup slideMenu dir

--- js/slideMenu-2.js
+++ js/slideMenu-2.js
@@ -1,18 +1,14 @@
 (function() {
-    Handlebars.registerHelper('visible', function(val) {
-        if (val === 'false' || !val) { return 'invisible' }
-    });
-
     var data = {
-            'link-1': {'displayName': 'home'},
-            'link-2': {'displayName': 'Collaboration', 'subLinks': [{'display': 'Buisness Unit Teams', 'url': '#'}, {'display': 'Projects', 'url': '#'},{'display': 'Communities', 'url': '#'}]},
-            'link-3': {'displayName': 'Employee Center', 'subLinks': [{'display': 'Benefits/Health', 'url': '#'}, {'display': 'Compensation', 'url': '#'},{'display': 'Orientation/Training', 'url': '#'},{'display': 'Work/Life', 'url': '#'}]},
-            'link-4': {'displayName': 'Workplace Services', 'subLinks': [{'display': 'Technology/Services', 'url': '#'}, {'display': 'Order/Reserve', 'url': '#'},{'display': 'Job Aids/Templates', 'url': '#'},{'display': 'Project Management', 'url': '#'}]},
-            'link-5': {'displayName': 'Company', 'subLinks': [{'display': 'News & Culture', 'url': '#'}, {'display': 'Leadership', 'url': '#'},{'display': 'About Buisness Units', 'url': '#'},{'display': 'Corporate Libraries', 'url': '#'}]}
+            'link-1': {'displayName': 'home', 'icon': 'icon-home'},
+            'link-2': {'displayName': 'Collaboration', 'icon': 'icon-proj', 'subLinks': [{'dispName': 'Buisness Unit Teams', 'url': '#'}, {'dispName': 'Projects', 'url': '#'},{'dispName': 'Communities', 'url': '#'}]},
+            'link-3': {'displayName': 'Employee Center', 'icon': 'icon-huma', 'subLinks': [{'dispName': 'Benefits/Health', 'url': '#'}, {'dispName': 'Compensation', 'url': '#'},{'dispName': 'Orientation/Training', 'url': '#'},{'dispName': 'Work/Life', 'url': '#'}]},
+            'link-4': {'displayName': 'Workplace Services', 'icon': 'icon-work', 'subLinks': [{'dispName': 'Technology/Services', 'url': '#'}, {'dispName': 'Order/Reserve', 'url': '#'},{'dispName': 'Job Aids/Templates', 'url': '#'},{'dispName': 'Project Management', 'url': '#'}]},
+            'link-5': {'displayName': 'Company', 'icon': 'icon-comp', 'subLinks': [{'dispName': 'News & Culture', 'url': '#'}, {'dispName': 'Leadership', 'url': '#'},{'dispName': 'About Business Units', 'url': '#'},{'dispName': 'Corporate Libraries', 'url': '#'}]}
         }
 
         // ------------------------------- START template content - (DO NOT touch) --------------------------- //
-        content = $('#entry-template').html(),//'<div id="nav-con" class="kill-pointer nav" data-id="nav-main" data-no><div class="mp-pusher" id="mp-pusher" data-no><nav id="mp-menu" class="mp-menu" data-no><div class="mp-level" data-id="global" id="global" data-no><ul data-no><li class="icon-global-menu" data-trigger data-id="sec-open" data-no><div class="mp-level secondary" data-id="nav-sec" data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" title="Global Menu" data-no>Global Menu</h4></span><a class="mp-back" href="#" data-trigger data-id="sec-close" data-no>back</a><ul><li class="nav-left-panel border-white border-solo-bot {{visible link-13.visible}}" data-no><a href="{{link-13.url}}" class="icon-inter" data-no>{{link-13.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-14.visible}}" data-no><a href="{{link-14.url}}" class="icon-intra" data-no>{{link-14.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-15.visible}}" data-no><a href="{{link-15.url}}" class="icon-extra" data-no>{{link-15.displayName}}</a></li></ul></div></li><li data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" data-no>intranet</h4></span></li><li class="nav-left-panel border-white border-solo-bot {{visible link-1.visible}}" data-no><a href="{{link-1.url}}" class="icon-home" data-no>{{link-1.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-2.visible}}" data-no><a href="{{link-2.url}}" class="icon-proj" data-no>{{link-2.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-3.visible}}" data-no><a href="{{link-3.url}}" class="icon-huma">{{link-3.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-4.visible}}" data-no><a href="{{link-4.url}}" class="icon-work" data-no>{{link-4.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot drop-down-custom {{visible link-5.visible}}" data-no><a href="{{link-5.url}}" data-trigger data-id="hidden-nav" class="icon-comp" data-no>{{link-5.displayName}}</a><ul class="mp-level-sub-drop bg-color-white" data-id="hidden-links" data-no><li class="nav-left-panel-sub {{visible link-6.visible}}" data-no><a href="{{link-6.url}}" class="indicator-border-left" data-no>{{link-6.displayName}}</a></li><li class="nav-left-panel-sub {{visible link-7.visible}}" data-no><a href="{{link-7.url}}" class="indicator-border-left" data-no>{{link-7.displayName}}</a></li><li class="nav-left-panel-sub {{visible link-8.visible}}" data-no><a href="{{link-8.url}}" class="indicator-border-left" data-no>{{link-8.displayName}}</a></li><li class="nav-left-panel-sub {{visible link-9.visible}}" data-no><a href="{{link-9.url}}" class="indicator-border-left" data-no>{{link-9.displayName}}</a></li></ul></li><li data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white height-force-min-40" data-no></h4></span></li><li class="nav-left-panel border-white border-solo-bot {{visible link-10.visible}}" data-no><a href="{{link-10.url}}" class="icon-sets" data-no>{{link-10.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-11.visible}}" data-no><a href="{{link-11.url}}" class="icon-term" data-no>{{link-11.displayName}}</a></li><li class="nav-left-panel border-white border-solo-bot {{visible link-12.visible}}" data-no><a href="{{link-12.url}}" class="icon-logs" data-no>{{link-12.displayName}}</a></li></ul></div><div class="mp-level" id="tertiary" data-id="nav-ter" data-no><span class="wrapit border-tb-lightgrey" data-no><h4 class="title border-pair-tb border-white" title="Tertiary Menu" data-no>Tertiary Menu</h4></span><a class="mp-back" href="#" data-trigger data-id="ter-close" data-no>back</a><ul><li class="nav-left-panel-sub {{visible link-6.visible}}" data-no><a href="{{link-6.url}}" class="indicator-border-left" data-no>{{link-6.displayName}}</a></li><li class="nav-left-panel-sub {{visible link-7.visible}}" data-no><a href="{{link-7.url}}" class="indicator-border-left" data-no>{{link-7.displayName}}</a></li><li class="nav-left-panel-sub {{visible link-8.visible}}" data-no><a href="{{link-8.url}}" class="indicator-border-left" data-no>{{link-8.displayName}}</a></li><li class="nav-left-panel-sub {{visible link-9.visible}}" data-no><a href="{{link-9.url}}" class="indicator-border-left" data-no>{{link-9.displayName}}</a></li></ul></div></nav></div></div>',
+        content = '<div class="kill-pointer nav" data-id="nav-main" data-no><div data-no><nav data-no><div class="list-container" data-id="global" id="global" data-no><ul data-no class="global-main"><li data-no class="trigger-container"><a href="#" class="menu-trigger main-trigger" data-trigger data-id="main" data-no></a></li><li class="menu-item selected" data-no><a class="{{link-1.icon}}" data-no>{{link-1.displayName}}</a></li><li class="menu-item" data-no><a data-trigger data-id="hidden-nav" data-link="link-2" class="{{link-2.icon}}" data-no>{{link-2.displayName}}</a></li><li class="menu-item" data-no><a data-trigger data-id="hidden-nav" data-link="link-3" class="{{link-3.icon}}" data-no>{{link-3.displayName}}</a></li><li class="menu-item" data-no><a data-trigger data-id="hidden-nav" data-link="link-4" class="{{link-4.icon}}" data-no>{{link-4.displayName}}</a></li><li class="menu-item" data-no><a data-trigger data-id="hidden-nav" data-link="link-5" class="{{link-5.icon}}" data-no>{{link-5.displayName}}</a></li></ul></div><div class="list-container" id="tertiary" data-id="nav-ter" data-no><div class="trigger-container" data-no><h4 data-trigger data-id="ter-close" class="title" title="Tertiary Menu" data-no>Main Menu</h4><a href="#" class="menu-trigger main-trigger" data-trigger data-id="main" data-no></a><div><ul class="tertiary-list" data-js="tertiary-list"><li class="menu-item selected" data-js="display-name" data-no><a class="" data-no></a></li></ul></div></nav></div></div>',
         template = Handlebars.compile(content),
         html = template(data),
         // ------------------------------- END template content - (DO NOT touch) ---------------------------- //
@@ -58,8 +54,19 @@
         closeTerpage = function() {  
             $('[data-id=nav-ter]').animate({left: '-310px'}, 400);
             $idMain.removeClass('open');
-        };
+        },
 
+        loadTertiarty = function(dataLink) {
+            $.each(data, function(i, el) {
+                if (i === dataLink) {
+                    $('[data-js=display-name] a').html(el.displayName).attr('class', el.icon);
+                    $('[data-js=sub]').remove()
+                    $.each(el.subLinks, function(i, elem) {
+                        $('[data-js=tertiary-list]').append('<li class="menu-item sub" data-js="sub" data-no><a href="' + elem.url + '" data-no>' + elem.dispName + '</a></li>');
+                    });
+                }
+            });
+        };
 
     // append template into DOM
     $('[data-target=template]').append(html);
@@ -81,7 +88,7 @@
                 break;
             case 'hidden-nav':
                 closeGlobalpage();
-                console.log(dataLink)
+                loadTertiarty(dataLink);
                 break;
             case 'ter-close':
                 openGlobalpage();
@@ -101,6 +108,7 @@
                 $('[data-id=hidden-links]').hide();
             }
         }
-    });   
+    });
+
 })();
 
